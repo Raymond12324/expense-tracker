05f38e9d7674dd851d4498156fe3e73dfe69983b
Randolph Raymond Carmona <raymondcarmonatejeda01@gmail.com>
2024-05-15 22:29:53 -0400

    auth info

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,15 +1,28 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-  return (
-    <div className='p-5  flex justify-between items-center border shadow'>
-        <Image src={'/logo.svg'} alt="logo" width={160} height={100}/>
-        <Button className="font-bold"> <Link href={'/sign-in'}>Get Started</Link> </Button>
-    </div>
-  )
-}
+  const { user, isSignedIn } = useUser();
 
-export default Header
+  return (
+    <div className="p-5  flex justify-between items-center border shadow">
+      <Image src={"/logo.svg"} alt="logo" width={160} height={100} />
+      {isSignedIn ? (
+        <>
+          <UserButton />
+        </>
+      ) : (
+        <Button className="font-bold">
+          {" "}
+          <Link href={"/sign-in"}>Get Started</Link>{" "}
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default Header;
