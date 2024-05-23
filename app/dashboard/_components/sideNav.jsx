224c81af3ae344e35f38efe542efa1c50d3b61b9
Randolph Raymond Carmona <raymondcarmonatejeda01@gmail.com>
2024-05-22 20:24:14 -0400

    chore: Update Header and sideNav components
    The Header component has been updated to remove commented code and enable the use of the `useUser` hook. The sideNav component has also been updated to remove unused imports and add a link to the homepage logo. These changes improve the functionality and visual presentation of the components.

--- app/dashboard/_components/sideNav.jsx
+++ app/dashboard/_components/sideNav.jsx
@@ -1,64 +1,63 @@
-"use client"
+"use client";
 
-import { LayoutGrid, PiggyBank, Receipt, Shield,Icon } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-
+import { LayoutGrid, PiggyBank, Receipt, Shield, Icon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function sideNav() {
+  const path = usePathname();
 
-    const path = usePathname();
-
-    const links = [
-        {
-            name: 'Dashboard',
-            href: '/dashboard',
-            icon: LayoutGrid
-        },
-        {
-            name: 'Budget',
-            href: '/dashboard/budget',
-            icon: PiggyBank
-        },
-        {
-            name: 'Expenses',
-            href: '/dashboard/expenses',
-            icon: Receipt
-        },
-        {
-            name: 'Upgrade',
-            href: '/dashboard/categories',
-            icon: Shield
-        },
-        // {
-        //     name: 'Reports',
-        //     href: '/dashboard/reports',
-        //     icon: 'reports'
-        // }
-    ]
+  const links = [
+    {
+      name: "Dashboard",
+      href: "/dashboard",
+      icon: LayoutGrid,
+    },
+    {
+      name: "Budget",
+      href: "/dashboard/budget",
+      icon: PiggyBank,
+    },
+    {
+      name: "Expenses",
+      href: "/dashboard/expenses",
+      icon: Receipt,
+    },
+    {
+      name: "Upgrade",
+      href: "/dashboard/categories",
+      icon: Shield,
+    },
+    // {
+    //     name: 'Reports',
+    //     href: '/dashboard/reports',
+    //     icon: 'reports'
+    // }
+  ];
 
   return (
-        <div className='h-screen border shadow-sm '>
-            <div className='mb-5 p-5 border-b-4 border-[#1ec67821]'>
-            <Image src='/logo.svg' alt='logo' width={160} height={100}/>   
-            </div>
-                
+    <div className="h-screen border shadow-sm ">
+      <div className="mb-5 p-5 border-b-4 border-[#1ec67821]">
+        <Link href={"/"}>
+          <Image src="/logo.svg" alt="logo" width={160} height={100} />
+        </Link>
+      </div>
 
-            {links.map((link, index) => (
-                <Link href={link.href} key={index}>
-               <h2 className={`flex gap-2 text-gray-600 font-medium p-5 cursor-pointer rounded-md hover:text-primary hover:bg-[#1ec67821] hover:font-semibold ${path === link.href ? "text-primary bg-[#1ec67821]" : ""}`}>
-                <link.icon/>
-                {link.name}
-               </h2>
-               </Link>
-            ))}
-         
-        </div>
-  )
+      {links.map((link, index) => (
+        <Link href={link.href} key={index}>
+          <h2
+            className={`flex gap-2 text-gray-600 font-medium p-5 cursor-pointer rounded-md hover:text-primary hover:bg-[#1ec67821] hover:font-semibold ${
+              path === link.href ? "text-primary bg-[#1ec67821]" : ""
+            }`}
+          >
+            <link.icon />
+            {link.name}
+          </h2>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
-export default sideNav
-
-
-      
+export default sideNav;
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,10 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    // <ClerkProvider>
-    <html lang="en">
-      <body className={outfit.className}>{children}</body>
-    </html>
-    // </ClerkProvider>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={outfit.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
